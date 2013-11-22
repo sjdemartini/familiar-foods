9a308b79238bb8029fabaf22f4e53ca1ea883fd5
Steven DeMartini <sjdemartini@gmail.com>
2013-11-22 01:38:40 -0800

    Add methods to load initial data from text file
    - Add tab-separated file to assets folder, from which initial food data
      can be loaded

--- src/edu/berkeley/cs160/familiarfoods/FamiliarFoodsDatabase.java
+++ src/edu/berkeley/cs160/familiarfoods/FamiliarFoodsDatabase.java
@@ -1,18 +1,28 @@
 package edu.berkeley.cs160.familiarfoods;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 import android.app.Application;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.util.Log;
 
 public class FamiliarFoodsDatabase extends Application {
@@ -45,13 +55,16 @@ public class FamiliarFoodsDatabase extends Application {
     protected HashMap<String, String> foodToCuisine;
 
     /** Local copy of food to description hashmap. */
-    protected HashMap<String, ArrayList<String>> foodToDesc;
+    protected HashMap<String, List<String>> foodToDesc;
 
     /** Local copy of food to photo hashmap. */
     protected HashMap<String, String> foodToPhoto;
 
     /** Local copy of cuisine to food hashmap. */
-    protected HashMap<String, ArrayList<String>> cuisineToFood;
+    protected HashMap<String, List<String>> cuisineToFood;
+
+    /** Dictates whether initial data should be loaded. */
+    protected boolean shouldLoadData = true;
 
     /**
      * All cuisines, stored in sorted order (for convenience when filtering).
@@ -65,8 +78,10 @@ public class FamiliarFoodsDatabase extends Application {
             "Italian",
             "Japanese",
             "Korean",
+            "Malaysian",
             "Mediterranean",
             "Mexican",
+            "Spanish",
             "Thai",
             "Vietnamese"
     };
@@ -74,17 +89,21 @@ public class FamiliarFoodsDatabase extends Application {
     @Override
     public void onCreate() {
         setupDatabaseFiles();
+        if (shouldLoadData) {
+            initializeDatabase();
+            shouldLoadData = false;
+        }
     }
 
     @SuppressWarnings("unchecked")
     protected void setupDatabaseFiles() {
         foodToCuisine = (HashMap<String, String>) loadObjectFromFile(
                 FOOD_TO_CUISINE_FILE);
-        foodToDesc = (HashMap<String, ArrayList<String>>) loadObjectFromFile(
+        foodToDesc = (HashMap<String, List<String>>) loadObjectFromFile(
                 FOOD_TO_DESCRIPTION_FILE);
         foodToPhoto = (HashMap<String, String>) loadObjectFromFile(
                 FOOD_TO_PHOTO_FILE);;
-        cuisineToFood = (HashMap<String, ArrayList<String>>) loadObjectFromFile(
+        cuisineToFood = (HashMap<String, List<String>>) loadObjectFromFile(
                 CUISINE_TO_FOOD_FILE);
 
         // If any value was not found from the file (in which case the file
@@ -95,7 +114,7 @@ public class FamiliarFoodsDatabase extends Application {
             saveFoodToCuisine();
         }
         if (foodToDesc == null) {
-            foodToDesc = new HashMap<String, ArrayList<String>>();
+            foodToDesc = new HashMap<String, List<String>>();
             saveFoodToDesc();
         }
         if (foodToPhoto == null) {
@@ -103,13 +122,23 @@ public class FamiliarFoodsDatabase extends Application {
             saveFoodToPhoto();
         }
         if (cuisineToFood == null) {
-            cuisineToFood = new HashMap<String, ArrayList<String>>();
+            cuisineToFood = new HashMap<String, List<String>>();
             for (String cuisine : cuisines) {
                 // Initialize the list of foods for each cuisine
                 cuisineToFood.put(cuisine, new ArrayList<String>());
             }
             saveCusineToFood();
         }
+    }
+
+    /**
+     * Initialize the database from a text file of data with food details.
+     */
+    protected void initializeDatabase() {
+        // Create an AsyncTask to initialize the database, since this will
+        // require a large amount of time (including fetching images from the
+        // web)
+        new DatabaseInitializer().execute();
     }
 
     /**
@@ -168,7 +197,7 @@ public class FamiliarFoodsDatabase extends Application {
         // Open a file output stream for this file
         FileOutputStream fos = null;
         try {
-            fos = openFileOutput(FOOD_TO_CUISINE_FILE, Context.MODE_PRIVATE);
+            fos = openFileOutput(filename, Context.MODE_PRIVATE);
         } catch (FileNotFoundException e) {
             // File should be created if it does not exist, so this only occurs
             // if there is an error
@@ -226,6 +255,7 @@ public class FamiliarFoodsDatabase extends Application {
      * Return a list of all cuisines (in alphabetical order) used in this app.
      * Useful for creating a list of cuisines to filter by (in search and in
      * Adventure Mode).
+     *
      * @return a sorted list of all cuisines.
      */
     public List<String> getAllCuisines() {
@@ -234,6 +264,7 @@ public class FamiliarFoodsDatabase extends Application {
 
     /**
      * Return a cuisine type for a given food.
+     *
      * @param foodName the food for which a cuisine type is being looked up.
      * @return a cuisine type for the given food, or null if food not found.
      */
@@ -243,14 +274,25 @@ public class FamiliarFoodsDatabase extends Application {
 
     /**
      * Get all the foods in the database for a given cuisine.
-     * @param cuisine The cuisine from which foods are being searched.
-     * @return a list of all food names belonging to the given cuisine, or
-     *  null if the cuisine can't be found.
+     *
+     * @param cuisine The cuisine from which foods are being fetched.
+     * @return a list of all food names belonging to the given cuisine.
      */
     public List<String> getFoodsForCuisine(String cuisine) {
-        return cuisineToFood.get(cuisine);
+         List<String> foods = cuisineToFood.get(cuisine);
+         if (foods == null) {
+             // If the cuisine was not found, create an empty list
+             foods = new ArrayList<String>();
+         }
+        return foods;
     }
 
+    /**
+     * Get all the foods in the database for the given cuisines.
+     *
+     * @param cuisines The cuisines from which foods are being fetched.
+     * @return a list of all food names belonging to the given cuisines.
+     */
     public List<String> getFoodsForCuisines(List<String> cuisines) {
         List<String> foods = new ArrayList<String>();
         if (cuisines == null) {
@@ -264,35 +306,182 @@ public class FamiliarFoodsDatabase extends Application {
 
     /**
      * Return a list of descriptors for a given food.
+     *
      * @param foodName the food for which a description is being looked up.
-     * @return a list of descriptors for the food.
+     * @return a list of descriptors for the food, or null if the food isn't
+     *  found.
      */
-    public ArrayList<String> getFoodDescription(String foodName) {
+    public List<String> getFoodDescription(String foodName) {
         return foodToDesc.get(foodName);
     }
 
     /**
+     * Return a Bitmap image of a given food.
+     *
+     * This method is useful if an image of a food is needed to be set for an
+     * ImageView.
+     *
+     * @param foodName The food for which an image is being looked up.
+     * @return A Bitmap image of the food, or null if the food isn't found.
+     */
+    public Bitmap getFoodPhoto(String foodName) {
+        Bitmap photo = null;
+        String filename = foodToPhoto.get(foodName);
+        if (filename == null) {
+            return photo;
+        }
+        FileInputStream fis;
+        try {
+            fis = openFileInput(filename);
+            photo = BitmapFactory.decodeStream(fis);
+            fis.close();
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, e.getMessage(), e);
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage(), e);
+            e.printStackTrace();
+        }
+        return photo;
+    }
+
+    /**
      * Add a food with its parameters to the database.
+     *
      * @param foodName The name of the food to add.
      * @param cuisine The name of the cuisine type.
      * @param description The descriptors for this food.
      * @param photoFile The filename for the photo.
      * @return whether the addition to the database was successful.
      */
-    public boolean addFoodToDatabase(String foodName, String cuisine,
-            ArrayList<String> description, String photoFile) {
-        if (foodToCuisine.containsKey(foodName)
-                || !cuisineToFood.containsKey(cuisine)) {
-            // If the food is already in the database or the cuisine does not
-            // exist, don't try to add the food
-            return false;
+    public void addFoodToDatabase(String foodName, String cuisine,
+            String[] descriptors, String photoFile) {
+        foodName = foodName.trim();
+        cuisine = cuisine.trim();
+        for (int i=0; i<descriptors.length; i++) {
+            descriptors[i] = descriptors[i].trim().toLowerCase(Locale.US);
+        }
+        if (foodToCuisine.containsKey(foodName)) {
+            throw new IllegalArgumentException(
+                    String.format(
+                            "The food %s is already in the database.", foodName));
+        }
+        if (! cuisineToFood.containsKey(cuisine)) {
+            throw new IllegalArgumentException(
+                    "The " + cuisine + " cuisine does not exist.");
         }
         foodToCuisine.put(foodName, cuisine);
-        foodToDesc.put(foodName, description);
+        foodToDesc.put(foodName, Arrays.asList(descriptors));
         foodToPhoto.put(foodName, photoFile);
-        ArrayList<String> foodsForCuisine = cuisineToFood.get(cuisine);
-        foodsForCuisine.add(foodName);
+        cuisineToFood.get(cuisine).add(foodName);
         saveAllData();
-        return true;
+        System.out.println("Added " + foodName);
+    }
+
+
+    /**
+     * An AsyncTask that loads initial data into the database.
+     */
+    private class DatabaseInitializer extends AsyncTask<Void, Void, Void> {
+        public static final String TAG = "FF_InitializeDB";
+
+        /**
+         * The filename for the food data.
+         *
+         * The file is organized with tab-separated columns:
+         * food name, cuisine, description words, photo link, similar foods
+         */
+        String DATA_FILENAME = "foods.tsv";
+
+        protected Void doInBackground(Void... params) {
+            loadDataFromFile();
+            return null;
+        }
+
+        /**
+         * Load data from the DATA_FILENAME file.
+         */
+        protected void loadDataFromFile() {
+            BufferedReader br = null;
+            String line, foodName, cuisine, photoUrl;
+            String[] descriptors;
+            String separator = "\\t";
+            try {
+                br = new BufferedReader(
+                        new InputStreamReader(getAssets().open(DATA_FILENAME)));
+                while ((line = br.readLine()) != null) {
+                    String[] foodDetails = line.split(separator);
+                    foodName = foodDetails[0].trim();
+                    if (foodToCuisine.containsKey(foodName)) {
+                        // This food is already in the db
+                        continue;
+                    }
+                    cuisine = foodDetails[1].trim();
+                    // Parse the descriptors from their comma-separated list:
+                    descriptors = foodDetails[2].split(",");
+                    photoUrl = foodDetails[3].trim();
+                    Bitmap photo = getPhotoFromUrl(photoUrl);
+                    String photoFilename = foodName + ".png";
+                    savePhotoToFile(photo, photoFilename);
+                    addFoodToDatabase(
+                            foodName, cuisine, descriptors, photoFilename);
+                }
+            } catch (FileNotFoundException e) {
+                Log.e(TAG, e.getMessage(), e);
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+            System.out.println("All foods loaded.");
+        }
+
+        /**
+         * Get a Bitmap for a photo fetched from the web.
+         * @param photoUrl The URL of the photo to be fetched.
+         * @return Bitmap of the photo.
+         */
+        protected Bitmap getPhotoFromUrl(String photoUrl) {
+            URL url = null;
+            try {
+                url = new URL(photoUrl);
+            } catch (MalformedURLException e) {
+                Log.e(TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+
+            // Make a new web request for the specific photo we want to display:
+            Bitmap bitmap = null;
+            try {
+                InputStream inputStream = url.openStream();
+                bitmap = BitmapFactory.decodeStream(inputStream);
+                inputStream.close();
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        /**
+         * Save the Bitmap image to internal storage with the given filename.
+         * @param filename Filename for photo.
+         * @param photo Bitmap image to save.
+         */
+        protected void savePhotoToFile(Bitmap photo, String filename) {
+            FileOutputStream fos;
+            try {
+                fos = openFileOutput(filename, Context.MODE_PRIVATE);
+                photo.compress(CompressFormat.PNG, 50, fos);
+                fos.flush();
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.e(TAG, e.getMessage(), e);
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+        }
     }
 }
