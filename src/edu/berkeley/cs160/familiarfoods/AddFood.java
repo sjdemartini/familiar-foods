f27db794d4d568fef8ead963d43cdfd04592b746
Tanninator <btan@Brians-MacBook-Pro-2.local>
2013-11-27 02:18:02 -0800

    Basic framework for LinkFood, AddFood. Connected to home page

---
+++ src/edu/berkeley/cs160/familiarfoods/AddFood.java
@@ -0,0 +1,77 @@
+package edu.berkeley.cs160.familiarfoods;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class AddFood extends Activity {
+
+    /** The database for this app. */
+    FamiliarFoodsDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adventure_mode);
+
+        // Show the Up button in the action bar.
+        setupActionBar();
+
+        // Get the database:
+        db = ((FamiliarFoodsDatabase) getApplication());
+
+      
+    }
+
+    /**
+     * Set up the {@link android.app.ActionBar}, if the API is available.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void setupActionBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.adventure_mode, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // This ID represents the Home or Up button. In the case of this
+                // activity, the Up button is shown. Use NavUtils to allow users
+                // to navigate up one level in the application structure. For
+                // more details, see the Navigation pattern on Android Design:
+                //
+                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+                //
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- src/edu/berkeley/cs160/familiarfoods/HomeActivity.java
+++ src/edu/berkeley/cs160/familiarfoods/HomeActivity.java
@@ -5,15 +5,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class HomeActivity extends Activity {
 
     public static final String TAG = "FF_Home";
+    Button adventureModeButton;
+    Button addFoodButton;
+    Button linkFoodButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        
+        adventureModeButton = (Button) findViewById(R.id.adventureModeButton);
+        addFoodButton = (Button) findViewById(R.id.addNewFoodButton);
+        linkFoodButton = (Button) findViewById(R.id.linkSimilarFoodsButton);
     }
 
     @Override
@@ -22,9 +31,40 @@ public class HomeActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+    public void startListeners() {
+    	adventureModeButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                startAdventureModeActivity(arg0);
+            }
+        });
+    	addFoodButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                startAddFoodActivity(arg0);
+            }
+        });
+    	linkFoodButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                startLinkFoodActivity(arg0);
+            }
+        });
+    }
 
     public void startAdventureModeActivity(View v){
         Intent openAdventureIntent = new Intent(this, AdventureMode.class);
+        startActivity(openAdventureIntent);
+    }
+
+    public void startAddFoodActivity(View v){
+        Intent openAdventureIntent = new Intent(this, AddFood.class);
+        startActivity(openAdventureIntent);
+    }
+    
+    public void startLinkFoodActivity(View v){
+        Intent openAdventureIntent = new Intent(this, LinkFood.class);
         startActivity(openAdventureIntent);
     }
 
