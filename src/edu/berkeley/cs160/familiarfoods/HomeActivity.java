434d8c6c9d4488a54a8a3aa1c0bbae8c80da745c
Steven DeMartini <sjdemartini@gmail.com>
2013-12-10 18:55:19 -0800

    Remove action bar from home screen and force all screens to be portrait

--- src/edu/berkeley/cs160/familiarfoods/HomeActivity.java
+++ src/edu/berkeley/cs160/familiarfoods/HomeActivity.java
@@ -2,6 +2,7 @@ package edu.berkeley.cs160.familiarfoods;
 
 import java.util.ArrayList;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -29,6 +30,11 @@ public class HomeActivity extends Activity implements OnItemClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Prevent the action bar from showing on the Home screen
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+
         setContentView(R.layout.activity_home);
 
         adventureModeButton = (ImageButton) findViewById(R.id.AdventureModeButton);
@@ -46,13 +52,6 @@ public class HomeActivity extends Activity implements OnItemClickListener {
         searchBar.setAdapter(adapter);
 
         startListeners();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
 
     public void startListeners() {
